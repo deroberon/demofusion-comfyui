9d5462a286f1a73e3fc3ecf28260118306f751b5
deroberon <andre.andrade@gmail.com>
2023-12-11 04:44:24 -0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,53 @@
+import os
+import subprocess
+import threading
+import sys
+import locale
+
+
+def handle_stream(stream, prefix):
+    stream.reconfigure(encoding=locale.getpreferredencoding(), errors='replace')
+    for msg in stream:
+        if prefix == '[!]' and ('it/s]' in msg or 's/it]' in msg) and ('%|' in msg or 'it [' in msg):
+            if msg.startswith('100%'):
+                print('\r' + msg, end="", file=sys.stderr),
+            else:
+                print('\r' + msg[:-1], end="", file=sys.stderr),
+        else:
+            if prefix == '[!]':
+                print(prefix, msg, end="", file=sys.stderr)
+            else:
+                print(prefix, msg, end="")
+
+def run_script(cmd, cwd='.'):
+    if len(cmd) > 0 and cmd[0].startswith("#"):
+        print(f"[ComfyUI-Manager] Unexpected behavior: `{cmd}`")
+        return 0
+
+    process = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
+
+    stdout_thread = threading.Thread(target=handle_stream, args=(process.stdout, ""))
+    stderr_thread = threading.Thread(target=handle_stream, args=(process.stderr, "[!]"))
+
+    stdout_thread.start()
+    stderr_thread.start()
+
+    stdout_thread.join()
+    stderr_thread.join()
+
+    return process.wait()
+
+
+my_path = os.path.dirname(__file__)
+requirements_path = os.path.join(my_path, "requirements.txt")
+
+print(f"## ComfyUI-Manager: installing dependencies")
+
+run_script([sys.executable, '-s', '-m', 'pip', 'install', '-r', requirements_path])
+
+from .demofusion import NODE_CLASS_MAPPINGS
+
+
+
+__all__ = ['NODE_CLASS_MAPPINGS']
+
---
+++ demofusion.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+my_dir = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(my_dir)
+from pipeline_demofusion_sdxl import DemoFusionSDXLPipeline
+sys.path.remove(my_dir)
+
+
+class Demofusion:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "positive": ("STRING", {
+                    "multiline": True, #True if you want the field to look like the one on the ClipTextEncode node
+                    "default": "Hello World!"
+                }),
+                "negative": ("STRING", {
+                    "multiline": True, #True if you want the field to look like the one on the ClipTextEncode node
+                    "default": ""
+                }),
+                "width": ("INT", {
+                    "default": 2048, 
+                    "min": 2048, #Minimum value
+                    "max": 4096, #Maximum value
+                    "step": 64, #Slider's step
+                    "display": "number" # Cosmetic only: display as "number" or "slider"
+                }),
+                "height": ("INT", {
+                    "default": 2048, 
+                    "min": 2048, #Minimum value
+                    "max": 4096, #Maximum value
+                    "step": 64, #Slider's step
+                    "display": "number" # Cosmetic only: display as "number" or "slider"
+                }),
+                "inference_steps": ("INT", {
+                    "default": 40, 
+                    "min": 1, #Minimum value
+                    "max": 100, #Maximum value
+                    "step": 1, #Slider's step
+                    "display": "number" # Cosmetic only: display as "number" or "slider"
+                }),
+                "cfg": ("FLOAT", {
+                    "default": 7.5,
+                    "min": 1.0,
+                    "max": 20.0,
+                    "step": 0.5,
+                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "number"}),
+                "seed": ("INT", {
+                    "default": 522, 
+                    "display": "number" # Cosmetic only: display as "number" or "slider"
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "execute"
+    CATEGORY = "tests"
+
+    def execute(self, positive, negative, width, height, inference_steps, cfg, seed):
+        model_ckpt = "stabilityai/stable-diffusion-xl-base-1.0"
+        pipe = DemoFusionSDXLPipeline.from_pretrained(model_ckpt, torch_dtype=torch.float16)
+        pipe = pipe.to("cuda")
+
+        generator = torch.Generator(device='cuda')
+        generator = generator.manual_seed(seed)
+
+        images = pipe(str(positive), negative_prompt=str(negative),
+              height=height, width=width, view_batch_size=4, stride=64,
+              num_inference_steps=inference_steps, guidance_scale=cfg,
+              cosine_scale_1=3, cosine_scale_2=1, cosine_scale_3=1, sigma=0.8,
+              multi_decoder=True, show_image=False#, lowvram=True
+             )
+        image=images[1]
+        image = image.convert("RGB")
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+
+        return (image,)
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "Demofusion": Demofusion
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Demofusion": "Demofusion"
+}
